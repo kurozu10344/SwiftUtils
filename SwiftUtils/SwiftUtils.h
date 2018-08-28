dffb8d3ab891ae1d8524a419702cd769327b0164
Hiroaki Tomiyoshi <htomiyosi@gmail.com>
2018-08-29 01:40:55 +0900

    Create project

---
+++ SwiftUtils/SwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtils.h
+//  SwiftUtils
+//
+//  Created by Hiroaki Tomiyoshi on 2018/08/29.
+//  Copyright © 2018年 Hiroaki Tomiyoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtils.
+FOUNDATION_EXPORT double SwiftUtilsVersionNumber;
+
+//! Project version string for SwiftUtils.
+FOUNDATION_EXPORT const unsigned char SwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtils/PublicHeader.h>
+
+
